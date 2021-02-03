aa5c052efd1652f355637d2686dfb6aad9d7e349
LBU <lgaz830304@gmail.com>
2021-02-04 00:39:36 +0800

    簡易servlet

---
+++ WebContent/login.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>    
+<!DOCTYPE html>
+<html>
+<head>
+<script src="<%=request.getContextPath()%>/plugin/jquery-3.5.1.min.js"></script>
+<meta charset="UTF-8">
+<title>登入頁面</title>
+</head>
+<body>
+登入: <br>
+<form METHOD="post"  ACTION="login" id="form">
+	帳號:<input type="text" name="account" id="account"><br>
+	密碼:<input type="text" name="password" id="password">
+</form>
+	<button id="send">送出</button>
+
+<script>
+$("#send").on("click", function() {
+	$("#form").submit();
+});
+</script>
+</body>
+</html>
---
+++ src/com/demo/controller/LoginController.java
@@ -0,0 +1,32 @@
+package com.demo.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginController extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		System.out.println("成功進入");
+//		String url ="";
+//		RequestDispatcher successView = req.getRequestDispatcher(url); 
+//		successView.forward(req, res);
+	
+	}
+	
+	
+
+}
