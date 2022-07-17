df5ca9c5b39e71d61773c638b23c40185b744742
AmazingSora <lgaz830304@gmail.com>
2022-07-18 04:05:08 +0800

    更新登入

--- src/main/java/idv/xeno/homeWork/security/config/SecurityConfig.java
+++ src/main/java/idv/xeno/homeWork/security/config/SecurityConfig.java
@@ -4,35 +4,67 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig implements WebMvcConfigurer {
+	// 密碼加密
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
+	// 調整至自訂畫面
+//	@Override
+//	public void addViewControllers(ViewControllerRegistry registry) {
+//		// TODO Auto-generated method stub
+////		WebMvcConfigurer.super.addViewControllers(registry);
+//		registry.addViewController("/").setViewName("index");
+//	}
+
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-		// TODO Auto-generated method stub
-		http.authorizeRequests()
-				// 設定放行名單
-				.antMatchers("/login","/sc/**").permitAll()
-				// 其餘路徑皆須進行驗證
-				.anyRequest().authenticated().and().formLogin().loginPage("/login").usernameParameter("hwUser")
-				.passwordParameter("hwPW")
-				.and().logout().logoutUrl("/logout").and()
-				.csrf().disable();
-
-		return http.build();
+	public UserDetailsService userDetailsService() {
+		InMemoryUserDetailsManager imudm = new InMemoryUserDetailsManager(
+				User.withUsername("admin").password(passwordEncoder().encode("admin")).authorities("auth").build(),
+				User.withUsername("amazingSora").password(passwordEncoder().encode("admin")).authorities("all","auth").build(),
+				User.withUsername("test").password(passwordEncoder().encode("test")).authorities("test").build()
+				);
+		return imudm;
 	}
-	 @Bean
-	    public WebSecurityCustomizer webSecurityCustomizer() {
-	        // 僅僅作為演示
-	        return (web) -> web.ignoring().antMatchers("/resources/**", "/static/**");
-	    }
+
+	// 授權控制
+//	@Bean
+//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//
+//		// TODO Auto-generated method stub
+//		http.authorizeRequests()
+//				// 設定放行名單
+//				.antMatchers("/login").permitAll()
+//				.antMatchers("/sc/**").hasAuthority("admin")
+//				// 其餘路徑皆須進行驗證
+//				.anyRequest()
+//				.authenticated();
+////		.and().formLogin().loginPage("/login").usernameParameter("hwUser")
+////				.passwordParameter("hwPW").defaultSuccessUrl("/main").failureUrl("/fail")
+////				.and()
+////				.logout().logoutUrl("/logout").and()
+////				.csrf().disable();
+//
+//		return http.build();
+//	}
+
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		// 僅僅作為演示
+		return (web) -> web.ignoring().antMatchers("/resources/**", "/static/**");
+	}
 
 }
--- src/main/java/idv/xeno/studyCircle/controller/SessionController.java
+++ src/main/java/idv/xeno/studyCircle/controller/SessionController.java
@@ -1,14 +1,11 @@
 package idv.xeno.studyCircle.controller;
 
-import javax.servlet.ServletContext;
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,17 +13,16 @@ import idv.xeno.studyCircle.model.dto.SessionDTO;
 
 @Controller
 public class SessionController {
-	private String sessionId ;
+	private String sessionId;
 
 	final static String MAIN = "jsp/sc/main";
 
 	@RequestMapping(value = "/sc/main")
-	public String main(HttpServletRequest request,HttpServletResponse response) {
-		HttpSession session =  request.getSession(true);
+	public String main(HttpServletRequest request, HttpServletResponse response) {
+		HttpSession session = request.getSession(true);
 
-	
 		sessionId = session.getId();
-		if(org.jooq.tools.StringUtils.isBlank(sessionId)) {
+		if (org.jooq.tools.StringUtils.isBlank(sessionId)) {
 			System.out.println("SESSION為空");
 			request.getSession(true);
 			sessionId = session.getId();
@@ -37,40 +33,30 @@ public class SessionController {
 		request.getSession().setAttribute("sessionId", sessionId);
 		request.getSession().setAttribute("t", "tttttttt");
 
-		
-
 		return MAIN;
-		
+
 	}
-	
+
 	@PostMapping(value = "/sc/setSession")
-	public String setSession(SessionDTO data,HttpServletRequest request,HttpServletResponse response) {
-		System.out.println("id ===>"+data.getId());
-		HttpSession session =  request.getSession();
-		Cookie cookie = new Cookie("JSESSIONID",data.getId().trim());		
+	public String setSession(SessionDTO data, HttpServletRequest request, HttpServletResponse response) {
+		System.out.println("id ===>" + data.getId());
+		HttpSession session = request.getSession();
+		Cookie cookie = new Cookie("JSESSIONID", data.getId().trim());
 		cookie.setMaxAge(session.getMaxInactiveInterval());
 		cookie.setPath(session.getServletContext().getContextPath());
 
 		response.addCookie(cookie);
 		return "redirect:" + "/sc/main";
-		
+
 	}
-	
+
 	@PostMapping(value = "/sc/setColor")
-	public String setBlue(String color,HttpServletRequest request,HttpServletResponse response) {
-		HttpSession session =  request.getSession();
-
-		System.out.println("color ==="+color);
+	public String setBlue(String color, HttpServletRequest request, HttpServletResponse response) {
+		System.out.println("color ===" + color);
 		request.getSession().setAttribute("color", color);
-		return main(request,response);
-		
-	}
-	
-	
+		return main(request, response);
 
-	
-	
-	
+	}
 
 	public String getSessionId() {
 		return sessionId;
